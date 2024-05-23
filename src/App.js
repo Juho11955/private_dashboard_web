89359a06619a19b3b6a1f7be896680b6bb433cad
mrk100409 <80090013+mrk100409@users.noreply.github.com>
2024-05-23 10:28:17 +0900

    Update App.js
    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   // axios.get('/test').then(resp=>console.log("resp")).catch((err)=>{console.log("error")}).finally(()=>{console.log("final")})
   return (
     <div className="App">
-      <p>test_page</p>
+      <p>test_page!!!</p>
       <div>
 	<span>test_contents</span>
       </div>

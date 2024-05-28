797652b28ae11d52c784ba16b06a9008378b99d6
go.cs <go.cs@ensecure.co.kr>
2024-05-28 16:29:40 +0900

    초기 커밋

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import axios from 'axios';
 function App() {
   // axios.get('/test').then(resp=>console.log("resp")).catch((err)=>{console.log("error")}).finally(()=>{console.log("final")})
   return (
-    <div className="App">
-      <p>test_page!!!</p>
-      <div>
-	<span>test_contents</span>
-      </div>
-    </div>
+    <></>
   );
 }
 

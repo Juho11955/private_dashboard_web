835e6721c6cf52c6db094807d6f57f51bb9a9d5b
Juho Lee <onu@Juhoui-Mac-Studio.local>
2024-05-29 22:20:37 +0900

    login init

---
+++ src/pages/Login.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+export default function Login() {
+
+useEffect(() => {
+    checkLogin();
+}, [])
+
+const checkLogin = () => {
+    return
+};
+
+const validateLogin = () => {
+    return
+    
+};
+
+const login = () => {
+    return
+
+};
+
+return (
+    <>
+    <div>
+        <div>logo</div>
+        <div></div>        
+        <div></div>        
+    </div>
+    </>
+)
+}
